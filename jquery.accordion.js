a70eb32ee9e3489a52c18bc90edab653fe9e46b5
冷子欲 <1058566903@qq.com>
2015-11-20 13:20:38 +0800

    remove bug

--- jquery.accordion.js
+++ jquery.accordion.js
@@ -9,11 +9,13 @@
 		var defaults = {
 				max: "620px",
 				min: "140px",
-				speed: "1000"
+				speed: "1000",
+				lazyload:true
 		}
 
 		var opts = $.extend(defaults, opts);
 
+		//懒加载
 		this.each(function(){
 			var t = $(this),
 					m = t.children(),
@@ -22,12 +24,20 @@
 			//触发事件
 			m.find(".active a").hide();
 			m.on("mouseenter","li",function(){
+
+				//如果开启懒加载并且data-src属性存在
+				if(opts.lazyload == true && typeof($(this).attr("data-src")) !== "undefined"){
+				  var src = $(this).attr("data-src");
+				  $(this).css("background-image","url(" + src +")").removeAttr("data-src");
+				}
+
 				$(this).addClass('active')
 							.animate({width:opts.max},opts.speed)
-							.find("a").fadeOut()
+							.children().fadeOut(opts.speed)
 							.parent().siblings().removeClass('active')
 							.animate({width:opts.min},opts.speed)
-							.find("a").show();
+							.children().show(opts.speed);
+
 			})
 		})
 	}
